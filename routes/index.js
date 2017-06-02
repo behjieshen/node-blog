d88d7892a41a89e164a4314e1f433d3737e3b166
behjieshen@gmail.com <behjieshen@gmail.com>
2017-06-02 19:12:05 +0800

    config mlab

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,13 @@ module.exports = function(router, passport) {
 
   // Home page blog post
   router.get('/', function(req, res, next) {
-    /*var db = req.db;
+    var db = req.db;
     var posts = db.get('posts');
     posts.find({}, {}, function(err, posts) {
       res.render('index', {
         "posts": posts
       });
-    })*/
-    res.render('index');
+    })
   });
 
   router.get('/master', function(req, res){
